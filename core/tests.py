7a6570183866694c0e0f5416eeed90eb6bde8537
Ronnie Wasukira <ronniewasukira299@gmail.com>
2026-03-14 13:36:45 +0300

    Fix accountt tests import -core modelfrom correct app

--- core/tests.py
+++ core/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-from .models import Product, Supplier, StockMovement
+from core.models import Product, Supplier, StockMovement
 
 User = get_user_model()
 
